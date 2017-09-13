58cc9a019fe6fc524127002bd95f1630f47c4434
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-09-13 11:57:59 -0700

    setup models index file for export & mongo connection. Setup MAIN index.js file & generated root & /api routes

---
+++ models/index.js
@@ -0,0 +1,8 @@
+//require mongoose & connect
+let mongoose = require('mongoose');
+
+// connect mongoose to database location to store data from end-points
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/wineapp_test");
+
+module.exports.Winery = require('./winery');
+module.exports.WineMap = require('./wineMap');
