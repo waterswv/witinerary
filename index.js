4221adda1bf014a4265b3a6969d389ed8b2a40fb
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-09-13 13:51:45 -0700

    refactored /api endpoint to leverage apiController

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ let express = require('express'),
 // generate a new express app and call it 'app'
 let app = express();
 
+// setup db instance to leverage models
 let db = require('./models');
+
+// setup instance of controllers to to leverage the controller functions
+let controllers = require('./controllers');
 
 // serve static files from public folder
 app.use(express.static(__dirname + 'public'));
@@ -21,7 +25,7 @@ app.use(express.static(__dirname + 'public'));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//routes
+//root route setup
 
 app.get('/', function(req, res){
   res.sendFile('views/index.html', {
@@ -30,16 +34,11 @@ app.get('/', function(req, res){
   console.log(__dirname);
 });
 
-app.get('/api', function(req, res) {
-  res.json({
-    description: "This API provides data to Witinerary",
-    gitHub: "https://github.com/waterswv/witinerary",
-    endPoints: "Coming Soon"
-  });
-});
+// API Controller Routes
+app.get('/api', controllers.api.index);
 
-//basic get wineries route
-app.get('/winery', function(req, res){
+// Winery Controller Routes
+app.get('/api/winery', function(req, res){
   db.Winery.find({}, function(err, wineries) {
     if (err){
       console.log('Error', err);
