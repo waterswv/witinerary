db0cdfcec838a6d91528d3154c13f9122c831ae0
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-09-17 22:10:48 -0700

    completed adding google maps directions view to map... needs design massaging

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -29,14 +29,15 @@ function wineMapIndexSuccess(wineMapData) {
   // Render winery data to page
   wineMapData.data[0].wineries.forEach(function(winery){
     renderMapMarker(winery);
-    let wineryDiv = `[data-winery-id=${winery._id}]`;
     // For each pool, render the events for that pool
+    let wineryDiv = `[data-winery-id=${winery._id}]`;
     // console.log(wineryDiv);
     });
     generateWineriesForMaps();
 }
 
 function mapWineryAddSuccess(wineryData) {
+  // grabbing winery added to map and using it to generate a new marker
   let winery = wineryData.wineries[wineryData.wineries.length-1];
   // console.log(winery);
   renderMapMarker(winery);
@@ -54,7 +55,8 @@ function mapWineryAddSuccess(wineryData) {
   function renderMapMarker(winery) {
     let marker = new google.maps.Marker({
       position: {lat: winery.maps.lat ,lng: winery.maps.long},
-      map: map
+      map: map,
+      title: winery.name // Adds hover pop-up on marker placed on map
     });
   }
 
@@ -71,11 +73,17 @@ function generateWineriesForMaps() {
      console.log("All Wineries Array: ", wineries[0]);
      console.log("All Mapped Wineries Array: ", mapWineries[0].data[0].wineries);
 
+    //  send waypoints object to calcRoute function for generating driving view...
+     let waypointsData = mapWineries[0].data[0].wineries.map((winery) => {return {location: winery.fullAddress}})
+     calcRoute(waypointsData); // TODO: figure out how to better arrange waypoints ... 
+     console.log(waypointsData);
      wineriesArr = wineries[0];
+
      console.log("Is this my all wineries Array... ", wineriesArr);
      mapWineriesArr = mapWineries[0].data[0].wineries.map((winery) => {return winery._id} );
 
     let wineriesNotOnMap = wineriesArr.filter(function(winery) {
+       // .includes function returns a boolean value depending on if an element is contained in an array
        return !mapWineriesArr.includes(winery._id);
      });
      console.log("The current wineries not on map: ", wineriesNotOnMap);
@@ -103,23 +111,19 @@ function generateWineriesForMaps() {
    });
 }
 
-$('.add-map-route').on('click', function(e) {
-  e.preventDefault();
-  calcRoute();
-  console.log('Do I work?');
-})
 
 
 
 
 // LEARNING GOOGLE MAPS DIRECTIONS >>>>
 
-function calcRoute() {
-  let winery1 = new google.maps.LatLng(38.6640092, -122.9342897);
-  let winery2 = new google.maps.LatLng(38.5706633, -122.7795547);
+function calcRoute(waypointsData) {
+  // let winery1 = new google.maps.LatLng(38.6640092, -122.9342897);
+  // let winery2 = new google.maps.LatLng(38.5706633, -122.7795547);
   let request = {
-    origin: winery1,
-    destination: winery2,
+    origin: {lat: 38.6205463, lng: -122.8997986},
+    destination: {lat: 38.6205463, lng: -122.8997986},
+    waypoints: waypointsData,
     // Note that Javascript allows us to access the constant
     // using square brackets and a string value as its
     // "property."
