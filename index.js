796ebf5f5ce38905f0b2ddd1f1827d5dcc7a8358
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2018-02-17 12:32:29 -0800

    imported graphQL & added test server on 4000 ... updated winemap Model to include start & end location lat/long fields ... updated seed.js to include start/end location examples

--- index.js
+++ index.js
@@ -25,6 +25,38 @@ app.use(express.static(__dirname + '/public'));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// RUNNING GRAPHQL ********************************************************
+      let graphqlHTTP = require('express-graphql');
+      let { buildSchema } = require('graphql');
+
+      // Construct a schema, using GraphQL schema language
+      let schema = buildSchema(`
+        type Query {
+          rollDice(numDice: Int!, numSides: Int): [Int]
+        }
+      `);
+
+      // The root provides a resolver function for each API endpoint
+      let root = {
+        rollDice: function ({numDice, numSides}) {
+          var output = [];
+          for (var i = 0; i < numDice; i++) {
+            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+          }
+          return output;
+        }
+      };
+
+      let graphApp = express();
+      graphApp.use('/graphql', graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+      }));
+      graphApp.listen(4000);
+      console.log('Running a GraphQL API server at localhost:4000/graphql');
+// END GRAPHQL ********************************************************
+
 //root route setup
 
 app.get('/', function(req, res){
